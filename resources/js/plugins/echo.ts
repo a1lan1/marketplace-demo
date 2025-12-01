7b41d4520ab27458d4c978cb6e9fd0c1f00342c4
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-01 21:24:10 +0300

    add laravel/reverb

--- resources/js/plugins/echo.ts
+++ resources/js/plugins/echo.ts
@@ -1,11 +1,11 @@
 import { configureEcho } from '@laravel/echo-vue'
 
 configureEcho({
-  broadcaster: 'pusher',
-  key: 'local',
-  wsHost: window.location.hostname,
-  wsPort: 6001,
-  forceTLS: false,
-  enabledTransports: ['ws'],
-  disableStats: true
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT,
+  wssPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss']
 })
