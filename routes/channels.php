bb8492524b57e089e9d74208cb217800200d5252
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-05 01:50:10 +0300

    add broadcast channels for chat, orders

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', fn ($user, $id): bool => (int) $user->id === (int) $id);
+Broadcast::channel('App.Models.User.{id}', fn (User $user, int $id): bool => $user->id === $id);
+
+Broadcast::channel('orders.{order}', fn (User $user, Order $order) => $user->can('view', $order));
+
+Broadcast::channel('chat.{order}', fn (User $user, Order $order) => $user->can('viewChat', $order));
