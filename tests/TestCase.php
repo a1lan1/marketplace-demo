3366a3af3b04f838d98d8cec21100c434dce9374
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-07 14:16:36 +0300

    add role seeder to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Override;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    #[Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(RoleSeeder::class);
+    }
 }
