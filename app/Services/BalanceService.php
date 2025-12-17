1068ab3b9659948fe0384ca6a2f5da4b6bd6bf73
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-17 14:29:18 +0300

    add lockForUpdate to user balance transaction

--- app/Services/BalanceService.php
+++ app/Services/BalanceService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Actions\Transactions\CreateTransactionAction;
 use App\Contracts\BalanceServiceInterface;
 use App\Enums\TransactionType;
 use App\Exceptions\InsufficientFundsException;
@@ -13,23 +14,28 @@ use Cknow\Money\Money;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
-class BalanceService implements BalanceServiceInterface
+readonly class BalanceService implements BalanceServiceInterface
 {
+    public function __construct(private CreateTransactionAction $createTransactionAction) {}
+
     /**
      * @throws Throwable
      */
     public function deposit(User $user, Money $amount, ?string $description = null): Transaction
     {
-        return DB::transaction(function () use ($user, $amount, $description) {
+        return DB::transaction(function () use ($user, $amount, $description): Transaction {
+            /** @var User $user */
+            $user = User::query()->lockForUpdate()->find($user->id);
+
             $user->balance = $user->balance->add($amount);
             $user->save();
 
-            return Transaction::create([
-                'user_id' => $user->id,
-                'amount' => $amount,
-                'type' => TransactionType::DEPOSIT,
-                'description' => $description,
-            ]);
+            return $this->createTransactionAction->execute(
+                user: $user,
+                amount: $amount,
+                type: TransactionType::DEPOSIT,
+                description: $description
+            );
         });
     }
 
@@ -39,20 +45,22 @@ class BalanceService implements BalanceServiceInterface
      */
     public function withdraw(User $user, Money $amount, ?string $description = null): Transaction
     {
-        if (! $this->hasSufficientFunds($user, $amount)) {
-            throw new InsufficientFundsException;
-        }
+        return DB::transaction(function () use ($user, $amount, $description): Transaction {
+            $user = User::query()->lockForUpdate()->find($user->id);
 
-        return DB::transaction(function () use ($user, $amount, $description) {
+            if (! $this->hasSufficientFunds($user, $amount)) {
+                throw new InsufficientFundsException;
+            }
+
             $user->balance = $user->balance->subtract($amount);
             $user->save();
 
-            return Transaction::create([
-                'user_id' => $user->id,
-                'amount' => $amount->negative(),
-                'type' => TransactionType::WITHDRAWAL,
-                'description' => $description,
-            ]);
+            return $this->createTransactionAction->execute(
+                user: $user,
+                amount: $amount,
+                type: TransactionType::WITHDRAWAL,
+                description: $description
+            );
         });
     }
 
