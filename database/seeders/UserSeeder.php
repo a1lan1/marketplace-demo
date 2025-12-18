2e805b178dcf913f161c3c2bb3f2ff4f3b7989a9
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-18 15:40:44 +0300

    add role states to user factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Enums\RoleEnum;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,42 +13,46 @@ class UserSeeder extends Seeder
     {
         User::factory()
             ->withAvatar()
+            ->withBaseRoles()
+            ->withAdminRole()
             ->withoutTwoFactor()
             ->create([
                 'name' => 'Admin',
                 'email' => 'test@example.com',
-            ])->assignRole(RoleEnum::ADMIN->value, RoleEnum::BUYER->value, RoleEnum::SELLER->value);
+            ]);
 
         User::factory()
             ->withAvatar()
+            ->withBaseRoles()
+            ->withManagerRole()
             ->withoutTwoFactor()
             ->create([
                 'name' => 'Manager',
                 'email' => 'demo@example.com',
-            ])->assignRole(RoleEnum::MANAGER->value, RoleEnum::BUYER->value, RoleEnum::SELLER->value);
+            ]);
 
         User::factory()
             ->withAvatar()
+            ->withBaseRoles()
             ->withoutTwoFactor()
             ->create([
                 'name' => 'Buyer',
                 'email' => 'buyer@example.com',
-            ])->assignRole(RoleEnum::BUYER->value, RoleEnum::SELLER->value);
+            ]);
 
         User::factory()
             ->withAvatar()
+            ->withBaseRoles()
             ->withoutTwoFactor()
             ->create([
                 'name' => 'Seller',
                 'email' => 'seller@example.com',
-            ])->assignRole(RoleEnum::BUYER->value, RoleEnum::SELLER->value);
+            ]);
 
         User::factory(5)
             ->withAvatar()
+            ->withBaseRoles()
             ->withoutTwoFactor()
-            ->afterCreating(function (User $user): void {
-                $user->assignRole(RoleEnum::BUYER->value, RoleEnum::SELLER->value);
-            })
             ->create();
     }
 }
