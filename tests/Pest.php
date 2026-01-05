29e30b15c05b231e1ecb92abac96c45f674e868a
Aleksandr Smirnov <genesboard@gmail.com>
2026-01-05 23:20:33 +0300

    add WithCachedConfig, WithCachedRoutes to pest config

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithCachedConfig;
+use Illuminate\Foundation\Testing\WithCachedRoutes;
 use Tests\TestCase;
 
 /*
@@ -16,11 +18,15 @@ use Tests\TestCase;
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
+    ->use(WithCachedRoutes::class)
+    ->use(WithCachedConfig::class)
     ->group('feature')
     ->in('Feature');
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
+    ->use(WithCachedRoutes::class)
+    ->use(WithCachedConfig::class)
     ->group('unit')
     ->in('Unit');
 
