7f22344ca9e00d49f2649ee998ab90f68553f235
Aleksandr Smirnov <genesboard@gmail.com>
2026-01-19 21:17:39 +0300

    add spatie/laravel-activitylog

---
+++ database/migrations/2026_01_17_113227_create_activity_log_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table): void {
+            $table->bigIncrements('id');
+            $table->string('log_name')->nullable();
+            $table->text('description');
+
+            $table->string('subject_type')->nullable();
+            $table->string('subject_id')->nullable();
+
+            $table->string('event')->nullable();
+
+            $table->string('causer_type')->nullable();
+            $table->string('causer_id')->nullable();
+
+            $table->json('properties')->nullable();
+            $table->uuid('batch_uuid')->nullable();
+            $table->timestamps();
+
+            $table->index('log_name');
+            $table->index(['subject_type', 'subject_id']);
+            $table->index(['causer_type', 'causer_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
+    }
+};
