79ffdd1425b4f5e7819a3e20b5116d94ff156ba3
Aleksandr Smirnov <genesboard@gmail.com>
2026-01-27 09:37:50 +0300

    add LogActivityJob

---
+++ app/Jobs/LogActivityJob.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class LogActivityJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function __construct(
+        protected Model $performedOn,
+        protected Model $causedBy,
+        protected string $description,
+        protected array $properties = []
+    ) {}
+
+    public function handle(): void
+    {
+        activity()
+            ->performedOn($this->performedOn)
+            ->causedBy($this->causedBy)
+            ->withProperties($this->properties)
+            ->log($this->description);
+    }
+}
